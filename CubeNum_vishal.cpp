9c746c80e6054d2fbe94a2ef6c254f3967f509b9
Vishal <gaba_vishal95@yahoo.co.in>
2019-10-17 06:31:04 -0700

    Added CubeNum_vishal.cpp file and commited

---
+++ CubeNum_vishal.cpp
@@ -0,0 +1,13 @@
+//Program to print cube of a number 
+//date created 17 October 2019
+//author vishal
+#include<stdio.h>
+int main(){
+	
+	int i;
+	printf("Enter a number to cube : \n");
+	scanf("%d",&i);
+	i=i*i*i;
+	printf("Its cube is : %d",i);
+	return 1;
+}
